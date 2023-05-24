1fce17b6a4b69c975a551a339c24d1cc89dddab2
mevanyouth <130651959+Dhevanfsy@users.noreply.github.com>
2023-05-24 23:09:29 +0700

    Add files via upload

--- register.php
+++ register.php
@@ -29,7 +29,7 @@ include 'header.php';
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="exampleInputPassword1">No Tepl</label>
+					<label for="exampleInputPassword1">No Telpon</label>
 					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="+62" name="telp" required >
 				</div>
 			</div>
